bc6239fa9e54cfdf2d983e29ee96e34a3ceb50e5
SoRa0000 <d.bayasgalan1230@gmail.com>
2023-07-10 17:24:25 +0800

    added user role parameters in generateToken() function && created adminControllers.js

--- utils/jwtUtils.js
+++ utils/jwtUtils.js
@@ -1,19 +1,25 @@
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 
-const generateToken = (payload) => {
-    return jwt.sign(payload,process.env.SECRET_KEY,
+const generateToken = (user, role) => {
+    return jwt.sign(
+        {
+            role,
+            userId: user._id,
+            username: user.username,
+        },
+        process.env.SECRET_KEY,
         {
             expiresIn: "1d",
-        });
-}
+        },
+    );
+};
 
 const verifyToken = (token) => {
-    try{
-        return jwt.verify(token,process.env.SECRET_KEY);
-    }
-    catch(err){
+    try {
+        return jwt.verify(token, process.env.SECRET_KEY);
+    } catch (err) {
         throw new Error("Invalid token");
     }
-}
+};
 
-module.exports = {generateToken,verifyToken};
+module.exports = { generateToken, verifyToken };
